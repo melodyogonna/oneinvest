2ca35b3715671c65f8a661a277eb57764b562120
Melody Daniel <melodyogonna@gmail.com>
2020-09-19 15:50:32 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    autoplay: true,
+    loop: true,
+    autoplayHoverPause: true,
+    animateOut: "fadeOut",
+  });
+});
